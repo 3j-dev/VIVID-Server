15bfc6fb65a5d138675abbdbcca9ac024e30752d
JeongSangByuk <jsb100800@naver.com>
2022-07-07 14:05:17 +0900

    [ADD] init project

---
+++ src/main/java/com/chicplay/mediaserver/MediaServerApplication.java
@@ -0,0 +1,13 @@
+package com.chicplay.mediaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chicplay/mediaserver/MediaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicplay.mediaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
