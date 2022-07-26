a20bcfe4958ffa9c931004621c60d4c9f241b22a
JeongSangByuk <jsb100800@naver.com>
2022-07-26 16:07:44 +0900

    [TEST][CP-46] test commit

--- src/main/java/com/chicplay/mediaserver/config/AwsS3Config.java
+++ src/main/java/com/chicplay/mediaserver/config/AwsS3Config.java
@@ -4,10 +4,13 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+@Slf4j
 @Configuration
 public class AwsS3Config {
 
